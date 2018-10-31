17f5e1a23dce293c47cbb8a2a6ac4cefff2e160f
Humza Mujib <hmujib@thoriumdigital.com>
2018-10-31 10:21:43 -0400

    Updates

---
+++ precache-manifest.924b0214740f321c86c5f95b5b55bce8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-weather-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "179a62970bcdc5a0cb3f",
+    "url": "/react-weather-app/static/css/main.011a861a.chunk.css"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "904601ec2b245593d7dd",
+    "url": "/react-weather-app/static/js/1.904601ec.chunk.js"
+  },
+  {
+    "revision": "72a6b7ef5541b624727a",
+    "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-weather-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "179a62970bcdc5a0cb3f",
+    "url": "/react-weather-app/static/js/main.179a6297.chunk.js"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-weather-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-weather-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-weather-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "904601ec2b245593d7dd",
+    "url": "/react-weather-app/static/css/1.7c6c9ec6.chunk.css"
+  },
+  {
+    "revision": "930731135f50d2518590ebc1c25885c3",
+    "url": "/react-weather-app/index.html"
+  }
+];
